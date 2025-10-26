79110a2553d713f7031cc718c86f3615a1691f6f
Dmytro Melnyk <179873458+demm05@users.noreply.github.com>
2025-10-26 13:04:15 +0000

    actully it's a bit easier now insertion has to be done

--- module_9/ex02/src/main.cpp
+++ module_9/ex02/src/main.cpp
@@ -1,72 +1,50 @@
 #include "../inc/UserInput.hpp"
 #include "../inc/PmergeMe.hpp"
 #include <iostream>
-#include <utility>
 
-// template <typename T>
-// std::vector<std::pair<T, T> >
-// merge_insert_sort(std::vector<std::pair<T, T> > &input) {
-// }
-
-// clang-format off
-/*
-
-1) vector<pair<int, int>>
-2) vector<pair<pair<int, int>, pair<int, int> > >
-3) vector<pair<pair<pair<int, int>, pair<int, int>>, pair<pair<int, int>,pair<int, int>> > >
- * */
-// clang-format on
+int depth = 0;
 
 template <typename T>
-std::vector<RecursivePair<T> > makePairs(std::vector<T> &input) {
-    std::vector<RecursivePair<T> > res;
-    res.reserve(input.size() / 2);
-    for (size_t i = 0; i < input.size(); i += 2) {
-        if (input[i] < input[i + 1]) {
-            res.push_back(RecursivePair(input[i], input[i + 1]));
-        } else {
-            res.push_back(RecursivePair(input[i + 1], input[i]));
-        }
-    }
-    return res;
-}
+void merge_insert_sort(std::vector<T> &items) {
+    bool has_straggler = 0;
+    int  straggler;
 
-template <typename T>
-void merge_insert_sort(std::vector<RecursivePair<T> > &items) {
     if (items.size() <= 1)
         return;
-    std::vector<RecursivePair<RecursivePair<T> > > pairs;
+    depth++;
+    if (items.size() % 2) {
+        straggler = items.back();
+        items.pop_back();
+        has_straggler = true;
+    }
+
+    std::vector<T> pairs;
+    std::vector<T> pend;
     pairs.reserve(items.size() / 2);
+    pend.reserve(items.size() / 2);
     for (size_t i = 0; i + 1 < items.size(); i += 2) {
         if (items[i] < items[i + 1]) {
-            pairs.push_back(RecursivePair(items[i], items[i + 1]));
+            pairs.push_back(items[i + 1]);
+            pend.push_back(items[i]);
         } else {
-            pairs.push_back(RecursivePair(items[i + 1], items[i]));
+            pairs.push_back(items[i]);
+            pend.push_back(items[i + 1]);
         }
     }
     merge_insert_sort(pairs);
+    std::cout << "depth: " << depth << "; main: [ " << pairs[0];
     for (size_t i = 1; i < pairs.size(); i++) {
-        std::cout << pairs[i];
+        std::cout << ", " << pairs[i];
     }
-    std::cout << std::endl;
+    std::cout << " ]\tpend: [ " << pend[0];
+    for (size_t i = 1; i < pend.size(); i++) {
+        std::cout << ", " << pend[i];
+    }
+    std::cout << " ]" << std::endl;
+    depth--;
 }
 
 int main(int argc, char const **argv) {
     auto res = processInput<int>(argc, argv);
-    bool has_straggler;
-    int  straggler;
-    if (res.size() % 2) {
-        straggler = res.back();
-        res.pop_back();
-        has_straggler = true;
-    }
-    auto pairs = makePairs(res);
-    // if (pairs.size() > 0)
-    //     std::cout << "[" << pairs[0].first << ", " << pairs[0].second << "]";
-    // for (size_t i = 1; i < pairs.size(); i++) {
-    //     std::cout << " | [" << pairs[i].first << ", " << pairs[i].second <<
-    //     "]";
-    // }
-    // std::cout << std::endl;
-    merge_insert_sort(pairs);
+    merge_insert_sort(res);
 }
